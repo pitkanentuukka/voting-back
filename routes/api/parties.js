e8223ebb1ffd10fae963bb36515bdb0823493003
tuukka <tuukka@thinkpad>
2020-05-20 13:51:52 +0300

    fixed party link validation

--- routes/api/parties.js
+++ routes/api/parties.js
@@ -17,15 +17,27 @@ router.get('/', cors(), (req, res) => {
 
 // validate party link
 router.get('/validate', cors(), (req, res) => {
-  let partyid = parseInt(req.query.partyId)
+  let partyid = parseInt(req.query.id)
   let link = req.query.link
   let sql = "select * from party where id = ? and link = ?"
   let inserts = [partyid, link]
-  connection.query(sql, inserts, function (error, results, fields) {
-    if (error) throw error;
-    res.json(results)
+  if (partyid && link) {
+    connection.query(sql, inserts, function (error, results, fields) {
+      if (error) {
+        res.status(500).json(error)
+      }
 
-  })
+      if (results[0]) {
+        res.status(200).json(results)
+
+      } else {
+        res.status(403).json({'msg': 'invalid link'})
+      }
+    })
+  } else {
+    res.status(400).json({'msg': 'bad request'})
+  }
+
 })
 
 
